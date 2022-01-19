09267a98281c9bc7a634e634aefb55696d4adc44
Kevin <kevin.j.wilkerson@gmail.com>
2022-01-19 17:07:09 -0600

    added connection between server.js and front end js

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 const { animals } = require('./data/animals.json');
+const res = require('express/lib/response');
+// set up static assets
+app.use(express.static('public'));
 
 
 
@@ -93,6 +96,22 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/animals', (req, res) => {
